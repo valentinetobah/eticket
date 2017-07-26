9062d55cb9338a1f393524b2c496fe266ed58ca4
Valentine Tobah <valentinetobah@gmail.com>
2017-07-26 15:10:47 +0200

    eticket

---
+++ src/java/model/PaginatorView.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author tobah
+ */
+
+ 
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+ 
+@ManagedBean(name="dtPaginatorView")
+@ViewScoped
+public class PaginatorView implements Serializable {
+     
+    private List<Car> cars;
+     
+    @ManagedProperty("#{carService}")
+    private CarService service;
+ 
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(100);
+    }
+     
+    public List<Car> getCars() {
+        return cars;
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
+}
+
